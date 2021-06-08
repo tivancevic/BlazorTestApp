f03ba03de9aac096210b7f6992a079b0bafb7b79
DESKTOP-JHVINQP\Tomislav <tomislav.ivancevic@tomsoft.hr>
2021-06-08 22:19:33 +0200

    Ştart of project

---
+++ Blazor_MySQL_testing/Models/PersonModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_MySQL_testing.Models
+{
+	public class PersonModel
+	{
+		//napisati "prop" i dva puta kliknut TAB
+		public int id { get; set; }
+		public string first_name { get; set; }
+		public string last_name { get; set; }
+		public string email { get; set; }
+	}
+}
---
+++ Blazor_MySQL_testing/Models/PhotoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_MySQL_testing.Models
+{
+	public class PhotoModel
+	{
+		public int id { get; set; }
+		public string url { get; set; }
+		public int person_id { get; set; }
+	}
+}
